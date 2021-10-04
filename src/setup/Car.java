ddb0731644e478538bfaa5a716b16ffd5578883b
Vinny <vindartahir1@gmail.com>
2021-10-04 16:00:15 -0700

    Created car class in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class Car {
+
+    // This is car class from master branch
+
+    // We will have conflict
+
+    // because they are different text in different branches for car
+}
