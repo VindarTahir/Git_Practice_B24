00a992cf0d9b9f98fa68fc1b51c6cd30b687661a
Vinny <vindartahir1@gmail.com>
2021-10-04 14:23:49 -0700

    Updated 2nd file

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,10 +3,11 @@ package setup;
 public class Second {
 
     public void m(){
-
+        System.out.println("First method");
     }
 
     public void m(int a){
-
+        System.out.println("Second method");
     }
+
 }
